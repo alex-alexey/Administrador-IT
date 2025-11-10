7ceee43aa223548daf704563be7a5c89811f11ee
alex-alexey <alex.gradinar09@gmail.com>
2025-11-10 18:55:55 +0100

    fix express session

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 // Backend básico Express + MongoDB (estructura inicial)
 
 
@@ -20,7 +21,11 @@ app.use(session({
   secret: 'intek-it-secret',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/intek-it',
+    ttl: 60 * 60 * 24, // 1 día
+  })
 }));
 app.use(cors());
 app.use(express.json());
